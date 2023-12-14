05d32f7730059944230d5d9a201c238598f65f16
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-12-14 23:46:17 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,1 +1,13 @@
-
+bool isPalindrome(int x) {
+    if(x<0)
+    return false;
+    long int p=x,num=0;
+    while(p!=0){
+        num=num*10+(p%10);
+        p=p/10;
+    }
+    if(num==x)
+    return true;
+    else
+    return false;
+}
