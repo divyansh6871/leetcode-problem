f9cacef04c0c96b94e6ea2cfc5f91f6dcbdcc5bb
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-12-20 23:25:20 +0530

    Update reverseString.c

--- reverseString.c
+++ reverseString.c
@@ -1,4 +1,4 @@
-
+// this is the function used to reverse the string using the function in which the argument is pointer to the first element in the character array
 void reverseString(char* s, int sSize) {
     for(int i=0;i<sSize/2;i++){
         char temp=s[i];
